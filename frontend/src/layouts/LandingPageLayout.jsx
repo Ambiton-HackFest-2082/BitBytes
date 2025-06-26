364ec3f470d63860109843f63a317c41901e6e7a
durlavdeo <durlavdeo2020@gmail.com>
2025-06-26 14:38:02 +0545

    context api setup

---
+++ frontend/src/layouts/LandingPageLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const LandinPageLayout = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
+
+export default LandinPageLayout;
---
+++ frontend/src/pages/LandingPage/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="h-screen w-screen bg-red-500">HOME PAGE CONTENT</div>;
+};
+
+export default Home;
