eef5f95ea650bc6c4a71157b7bbc0c3b66f64032
santu yadav <115726207+yadavsantu@users.noreply.github.com>
2025-06-26 13:50:52 +0545

    mongo atlas connected to backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+// index.js
+import app from './app.js';
+import connectDB from './db/db.config.js';
+
+const PORT = process.env.PORT || 5000;
+connectDB().then(() => {
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
+}
+).catch((err) => {
+  console.error('❌ Failed to connect to the database:', err);
+  process.exit(1);
+});
+
